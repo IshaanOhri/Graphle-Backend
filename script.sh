b3993b878be2b8ef6855c8f10f9e0bbb59d9f16b
Ishaan Ohri <ishaan99ohri@gmail.com>
2020-10-01 17:44:43 +0530

    Node server setup

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+echo "================="
+echo "Pushing to GitHub"
+echo "================="
+git add .
+echo "Enter commit message"
+read commit
+git commit -m "$commit"
+git checkout ishaan
+git push origin ishaan
