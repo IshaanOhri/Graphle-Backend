02234a281d82a1bcfc93311f7cdce6b8b95c9d6b
Ishaan Ohri <ishaan99ohri@gmail.com>
2021-03-28 16:55:30 +0530

    Fixed copy issue

--- src/api/controllers/channel.ts
+++ src/api/controllers/channel.ts
@@ -19,15 +19,15 @@ import { JoinedParticipant } from '../../interfaces/JoinedParticipant';
 
 // Initialize Pusher
 const pusher: Pusher = new Pusher({
-	appId: String(process.env.PUSHER_APP_ID),
-	key: String(process.env.PUSHER_KEY),
-	secret: String(process.env.PUSHER_SECRET),
-	cluster: String(process.env.PUSHER_CLUSTER),
-	useTLS: true
+    appId: String(process.env.PUSHER_APP_ID),
+    key: String(process.env.PUSHER_KEY),
+    secret: String(process.env.PUSHER_SECRET),
+    cluster: String(process.env.PUSHER_CLUSTER),
+    useTLS: true
 });
 
 const projectId: string = String(process.env.GOOGLE_CLOUD_PROJECT_ID);
-const keyFilename: string = 'learning-283013-9b7177e8ca72.json';
+const keyFilename: string = 'kubernetes-learn-294607-b0b5bf3dfb5f.json';
 
 const storage: Storage = new Storage({ projectId, keyFilename });
 const client = new TextToSpeechClient({ projectId, keyFilename });
@@ -35,372 +35,372 @@ const client = new TextToSpeechClient({ projectId, keyFilename });
 const bucket = storage.bucket(String(process.env.GOOGLE_CLOUD_BUCKET));
 
 const createChannel = async (req: Request, res: Response) => {
-	if (!req.body.channelName || !req.body.instructorName) {
-		res.status(400).send({
-			success: false,
-			code: code.wrongParameters,
-			message: message.wrongParameters
-		});
-		return;
-	}
+    if (!req.body.channelName || !req.body.instructorName) {
+        res.status(400).send({
+            success: false,
+            code: code.wrongParameters,
+            message: message.wrongParameters
+        });
+        return;
+    }
 
-	let channelID = '';
+    let channelID = '';
 
-	let unique: boolean = false;
+    let unique: boolean = false;
 
-	while (!unique) {
-		channelID = cryptoRandomString({ length: 6, type: 'distinguishable' });
-		const exist = await Channel.findOne({ channelID });
-		if (!exist) {
-			unique = true;
-		}
-	}
+    while (!unique) {
+        channelID = cryptoRandomString({ length: 6, type: 'distinguishable' });
+        const exist = await Channel.findOne({ channelID });
+        if (!exist) {
+            unique = true;
+        }
+    }
 
-	const channelInfo: ChannelInterface = {
-		channelID,
-		channelName: req.body.channelName,
-		instructorName: req.body.instructorName,
-		participants: []
-	};
+    const channelInfo: ChannelInterface = {
+        channelID,
+        channelName: req.body.channelName,
+        instructorName: req.body.instructorName,
+        participants: []
+    };
 
-	try {
-		await Channel.create(channelInfo);
-	} catch (err) {
-		logger.error(err);
-		res.status(500).send({
-			success: false,
-			code: code.channelCreation,
-			message: message.channelCreation
-		});
-		return;
-	}
+    try {
+        await Channel.create(channelInfo);
+    } catch (err) {
+        logger.error(err);
+        res.status(500).send({
+            success: false,
+            code: code.channelCreation,
+            message: message.channelCreation
+        });
+        return;
+    }
 
-	const storyInfo: StoryInterface = {
-		channelID,
-		channelName: req.body.channelName,
-		instructorName: req.body.instructorName,
-		story: []
-	};
+    const storyInfo: StoryInterface = {
+        channelID,
+        channelName: req.body.channelName,
+        instructorName: req.body.instructorName,
+        story: []
+    };
 
-	try {
-		await Story.create(storyInfo);
-	} catch (err) {
-		logger.error(err);
-		res.status(500).send({
-			success: false,
-			code: code.channelCreation,
-			message: message.channelCreation
-		});
-		return;
-	}
+    try {
+        await Story.create(storyInfo);
+    } catch (err) {
+        logger.error(err);
+        res.status(500).send({
+            success: false,
+            code: code.channelCreation,
+            message: message.channelCreation
+        });
+        return;
+    }
 
-	res.send({
-		success: true,
-		channelInfo: {
-			channelID: channelInfo.channelID,
-			channelName: channelInfo.channelName,
-			instructorName: channelInfo.instructorName
-		}
-	});
+    res.send({
+        success: true,
+        channelInfo: {
+            channelID: channelInfo.channelID,
+            channelName: channelInfo.channelName,
+            instructorName: channelInfo.instructorName
+        }
+    });
 };
 
 const reciteStory = async (req: Request, res: Response) => {
-	if (!req.body.channelID || !req.body.query) {
-		res.status(400).send({
-			success: false,
-			code: code.wrongParameters,
-			message: message.wrongParameters
-		});
-		return;
-	}
+    if (!req.body.channelID || !req.body.query) {
+        res.status(400).send({
+            success: false,
+            code: code.wrongParameters,
+            message: message.wrongParameters
+        });
+        return;
+    }
 
-	const { channelID, query } = req.body;
+    const { channelID, query } = req.body;
 
-	// Text to Speech
+    // Text to Speech
 
-	const settings: any = {
-		audioConfig: { audioEncoding: 'MP3', speakingRate: 0.7 },
-		input: {
-			text: query
-		},
-		voice: { languageCode: 'en-US', name: 'en-IN-Wavenet-A' }
-	};
+    const settings: any = {
+        audioConfig: { audioEncoding: 'MP3', speakingRate: 0.7 },
+        input: {
+            text: query
+        },
+        voice: { languageCode: 'en-US', name: 'en-IN-Wavenet-A' }
+    };
 
-	const [response] = await client.synthesizeSpeech(settings);
+    const [response] = await client.synthesizeSpeech(settings);
 
-	const blob = bucket.file(`${channelID}/${Date.now()}.mp3`);
+    const blob = bucket.file(`${channelID}/${Date.now()}.mp3`);
 
-	const blobStream = blob.createWriteStream({
-		metadata: {
-			contentType: 'audio/mpeg'
-		}
-	});
+    const blobStream = blob.createWriteStream({
+        metadata: {
+            contentType: 'audio/mpeg'
+        }
+    });
 
-	await blobStream.on('error', (err) => {
-		console.log('err');
-		console.log(err);
-	});
+    await blobStream.on('error', (err) => {
+        console.log('err');
+        console.log(err);
+    });
 
-	const audio: string = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
-	await blobStream.on('finish', () => {
-		// Make the image public to the web (since we'll be displaying it in browser)
-		blob.makePublic().then(() => {});
+    const audio: string = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+    await blobStream.on('finish', () => {
+        // Make the image public to the web (since we'll be displaying it in browser)
+        blob.makePublic().then(() => {});
 
-		const urls: string[] = [];
+        const urls: string[] = [];
 
-		const bingAPIkey: string = String(process.env.BING_API_KEY);
+        const bingAPIkey: string = String(process.env.BING_API_KEY);
 
-		fetch(`https://api.bing.microsoft.com/v7.0/images/search?q=${query.toLowerCase()}&count=6&imageType=Clipart`, {
-			method: 'GET',
-			headers: {
-				'Ocp-Apim-Subscription-Key': bingAPIkey
-			}
-		})
-			.then((resp) => resp.json())
-			.then(async (json) => {
-				const j = json.value;
-				_.forEach(j, (element) => {
-					urls.push(element.thumbnailUrl);
-				});
+        fetch(`https://api.bing.microsoft.com/v7.0/images/search?q=${query.toLowerCase()}&count=6&imageType=Clipart`, {
+            method: 'GET',
+            headers: {
+                'Ocp-Apim-Subscription-Key': bingAPIkey
+            }
+        })
+            .then((resp) => resp.json())
+            .then(async (json) => {
+                const j = json.value;
+                _.forEach(j, (element) => {
+                    urls.push(element.thumbnailUrl);
+                });
 
-				// Send to all active connections
-				try {
-					pusher.trigger(`presence-${channelID}`, 'my-event', {
-						message: {
-							query,
-							urls,
-							audio
-						}
-					});
-				} catch (err) {
-					logger.error(err);
-				}
+                // Send to all active connections
+                try {
+                    pusher.trigger(`presence-${channelID}`, 'my-event', {
+                        message: {
+                            query,
+                            urls,
+                            audio
+                        }
+                    });
+                } catch (err) {
+                    logger.error(err);
+                }
 
-				const snippet: StorySnippet = {
-					query,
-					urls,
-					createdAt: Date.now()
-				};
+                const snippet: StorySnippet = {
+                    query,
+                    urls,
+                    createdAt: Date.now()
+                };
 
-				try {
-					const story: any = await Story.findOne({ channelID });
-					story.story.push(snippet);
+                try {
+                    const story: any = await Story.findOne({ channelID });
+                    story.story.push(snippet);
 
-					await story.save();
-				} catch (err) {
-					logger.error(err);
-					logger.error({
-						success: false,
-						code: code.storyBackup,
-						message: message.storyBackup
-					});
-				}
-			})
-			.catch((err) => {
-				logger.error(err);
-			});
+                    await story.save();
+                } catch (err) {
+                    logger.error(err);
+                    logger.error({
+                        success: false,
+                        code: code.storyBackup,
+                        message: message.storyBackup
+                    });
+                }
+            })
+            .catch((err) => {
+                logger.error(err);
+            });
 
-		res.send({
-			success: true,
-			query
-		});
-	});
+        res.send({
+            success: true,
+            query
+        });
+    });
 
-	await blobStream.end(response.audioContent);
+    await blobStream.end(response.audioContent);
 };
 
 const joinChannel = async (req: Request, res: Response) => {
-	if (!req.query.channelID || !req.query.participantID) {
-		res.render('error/invalidSessionID');
-		return;
-	}
-	let { channelID } = req.query;
+    if (!req.query.channelID || !req.query.participantID) {
+        res.render('error/invalidSessionID');
+        return;
+    }
+    let { channelID } = req.query;
 
-	channelID = String(channelID).toUpperCase();
+    channelID = String(channelID).toUpperCase();
 
-	// eslint-disable-next-line prefer-destructuring
-	const user: any = req.user;
+    // eslint-disable-next-line prefer-destructuring
+    const user: any = req.user;
 
-	const participant: JoinedParticipant = {
-		id: user.participantID,
-		name: user.displayName,
-		image: user.image
-	};
+    const participant: JoinedParticipant = {
+        id: user.participantID,
+        name: user.displayName,
+        image: user.image
+    };
 
-	const exist: any = await Channel.findOne({ channelID });
+    const exist: any = await Channel.findOne({ channelID });
 
-	if (!exist) {
-		res.render('error/invalidSessionID');
-	} else if (_.find(exist.participants, { id: user.participantID })) {
-		res.render('error/alreadyJoined');
-	} else {
-		try {
-			exist.participants.push(participant);
-			exist.save();
-			res.render('channel', {
-				instructorName: exist.instructorName,
-				channelName: exist.channelName,
-				sessionID: channelID,
-				participantID: user.participantID,
-				name: user.displayName,
-				image: user.image,
-				layout: 'channel'
-			});
-		} catch (err) {
-			logger.error(err);
-			res.render('error/500');
-		}
-	}
+    if (!exist) {
+        res.render('error/invalidSessionID');
+    } else if (_.find(exist.participants, { id: user.participantID })) {
+        res.render('error/alreadyJoined');
+    } else {
+        try {
+            exist.participants.push(participant);
+            exist.save();
+            res.render('channel', {
+                instructorName: exist.instructorName,
+                channelName: exist.channelName,
+                sessionID: channelID,
+                participantID: user.participantID,
+                name: user.displayName,
+                image: user.image,
+                layout: 'channel'
+            });
+        } catch (err) {
+            logger.error(err);
+            res.render('error/500');
+        }
+    }
 };
 
 const leaveChannel = async (req: Request, res: Response) => {
-	if (!req.query.channelID || !req.query.participantID) {
-		res.render('error/invalidSessionID');
-		return;
-	}
+    if (!req.query.channelID || !req.query.participantID) {
+        res.render('error/invalidSessionID');
+        return;
+    }
 
-	const { channelID, participantID } = req.query;
+    const { channelID, participantID } = req.query;
 
-	// eslint-disable-next-line prefer-destructuring
-	const user: any = req.user;
+    // eslint-disable-next-line prefer-destructuring
+    const user: any = req.user;
 
-	const participant: JoinedParticipant = {
-		id: user.participantID,
-		name: user.displayName,
-		image: user.image
-	};
+    const participant: JoinedParticipant = {
+        id: user.participantID,
+        name: user.displayName,
+        image: user.image
+    };
 
-	const exist: any = await Channel.findOne({ channelID });
+    const exist: any = await Channel.findOne({ channelID });
 
-	if (!exist) {
-		res.render('error/invalidSessionID');
-	} else if (!_.find(exist.participants, { id: user.participantID })) {
-		res.render('error/notJoined');
-	} else {
-		try {
-			const current: any = _.find(exist.participants, { id: user.participantID });
-			const index = exist.participants.indexOf(current);
-			if (index > -1) {
-				exist.participants.splice(index, 1);
-			}
-			exist.save();
-			res.redirect('/dashboard');
-		} catch (err) {
-			logger.error(err);
-			res.render('error/500');
-		}
-	}
+    if (!exist) {
+        res.render('error/invalidSessionID');
+    } else if (!_.find(exist.participants, { id: user.participantID })) {
+        res.render('error/notJoined');
+    } else {
+        try {
+            const current: any = _.find(exist.participants, { id: user.participantID });
+            const index = exist.participants.indexOf(current);
+            if (index > -1) {
+                exist.participants.splice(index, 1);
+            }
+            exist.save();
+            res.redirect('/dashboard');
+        } catch (err) {
+            logger.error(err);
+            res.render('error/500');
+        }
+    }
 };
 
 const add = async (req: Request, res: Response) => {
-	if (!req.query.channelID || !req.query.participantID) {
-		res.send({
-			success: false
-		});
-		return;
-	}
+    if (!req.query.channelID || !req.query.participantID) {
+        res.send({
+            success: false
+        });
+        return;
+    }
 
-	let { channelID, participantID } = req.query;
+    let { channelID, participantID } = req.query;
 
-	channelID = String(channelID).toUpperCase();
+    channelID = String(channelID).toUpperCase();
 
-	// eslint-disable-next-line prefer-destructuring
-	const user: any = req.user;
+    // eslint-disable-next-line prefer-destructuring
+    const user: any = req.user;
 
-	const participant: JoinedParticipant = {
-		id: user.participantID,
-		name: user.displayName,
-		image: user.image
-	};
+    const participant: JoinedParticipant = {
+        id: user.participantID,
+        name: user.displayName,
+        image: user.image
+    };
 
-	const exist: any = await Channel.findOne({ channelID });
+    const exist: any = await Channel.findOne({ channelID });
 
-	if (!exist) {
-		res.send({
-			success: false
-		});
-	} else if (_.find(exist.participants, { id: user.participantID })) {
-		res.send({
-			success: false
-		});
-	} else {
-		try {
-			exist.participants.push(participant);
-			exist.save();
-			res.send({
-				success: true
-			});
-			return;
-		} catch (err) {
-			logger.error(err);
-			res.send({
-				success: false
-			});
-		}
-	}
+    if (!exist) {
+        res.send({
+            success: false
+        });
+    } else if (_.find(exist.participants, { id: user.participantID })) {
+        res.send({
+            success: false
+        });
+    } else {
+        try {
+            exist.participants.push(participant);
+            exist.save();
+            res.send({
+                success: true
+            });
+            return;
+        } catch (err) {
+            logger.error(err);
+            res.send({
+                success: false
+            });
+        }
+    }
 };
 
 const remove = async (req: Request, res: Response) => {
-	if (!req.query.channelID || !req.query.participantID) {
-		res.send({
-			success: false
-		});
-		return;
-	}
+    if (!req.query.channelID || !req.query.participantID) {
+        res.send({
+            success: false
+        });
+        return;
+    }
 
-	let { channelID, participantID } = req.query;
+    let { channelID, participantID } = req.query;
 
-	channelID = String(channelID).toUpperCase();
+    channelID = String(channelID).toUpperCase();
 
-	const exist: any = await Channel.findOne({ channelID });
+    const exist: any = await Channel.findOne({ channelID });
 
-	if (!exist) {
-		res.send({
-			success: false
-		});
-	} else if (!_.find(exist.participants, { id: participantID })) {
-		res.send({
-			success: false
-		});
-	} else {
-		try {
-			const current: any = _.find(exist.participants, { id: participantID });
-			const index = exist.participants.indexOf(current);
-			if (index > -1) {
-				exist.participants.splice(index, 1);
-			}
-			exist.save();
-			res.send({
-				success: true
-			});
-			return;
-		} catch (err) {
-			logger.error(err);
-			res.send({
-				success: false
-			});
-		}
-	}
+    if (!exist) {
+        res.send({
+            success: false
+        });
+    } else if (!_.find(exist.participants, { id: participantID })) {
+        res.send({
+            success: false
+        });
+    } else {
+        try {
+            const current: any = _.find(exist.participants, { id: participantID });
+            const index = exist.participants.indexOf(current);
+            if (index > -1) {
+                exist.participants.splice(index, 1);
+            }
+            exist.save();
+            res.send({
+                success: true
+            });
+            return;
+        } catch (err) {
+            logger.error(err);
+            res.send({
+                success: false
+            });
+        }
+    }
 };
 
 const participants = async (req: Request, res: Response) => {
-	if (!req.query.channelID) {
-		res.status(400).send({
-			success: false,
-			code: code.wrongParameters,
-			message: message.wrongParameters
-		});
-		return;
-	}
-	const { channelID } = req.query;
+    if (!req.query.channelID) {
+        res.status(400).send({
+            success: false,
+            code: code.wrongParameters,
+            message: message.wrongParameters
+        });
+        return;
+    }
+    const { channelID } = req.query;
 
-	const channel: any = await Channel.findOne({ channelID });
+    const channel: any = await Channel.findOne({ channelID });
 
-	res.send({
-		success: true,
-		channelID,
-		participants: channel.participants
-	});
+    res.send({
+        success: true,
+        channelID,
+        participants: channel.participants
+    });
 };
 
 export { createChannel, reciteStory, joinChannel, leaveChannel, add, remove, participants };
