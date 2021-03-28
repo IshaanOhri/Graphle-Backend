6dd88cf777faf74e84a28be38fe2e3f4e09baacc
Ishaan Ohri <ishaan99ohri@gmail.com>
2021-03-28 16:37:53 +0530

    Changed cloudbuild

--- script.sh
+++ script.sh
@@ -8,5 +8,5 @@ git add .
 echo "Enter commit message"
 read commit
 git commit -m "$commit"
-git checkout master
-git push origin master
+git checkout nlp
+git push origin nlp
--- src/config/passport.ts
+++ src/config/passport.ts
@@ -3,44 +3,44 @@ import logger from '../logger/config';
 import Participant from '../modals/Participant';
 
 module.exports = (passport: any) => {
-	passport.use(
-		new GoogleStrategy.Strategy(
-			{
-				clientID: String(process.env.GOOGLE_CLIENT_ID),
-				clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
-				callbackURL: String(process.env.GOOGLE_CALLBACK)
-			},
-			async (accessToken, refreshToken, profile, done) => {
-				const newUser = {
-					participantID: profile.id,
-					displayName: profile.displayName,
-					firstName: profile.name?.givenName,
-					lastName: profile.name?.familyName,
-					image: profile.photos![0].value
-				};
+    passport.use(
+        new GoogleStrategy.Strategy(
+            {
+                clientID: String(process.env.GOOGLE_CLIENT_ID),
+                clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
+                callbackURL: String(process.env.GOOGLE_CALLBACK)
+            },
+            async (accessToken, refreshToken, profile, done) => {
+                const newUser = {
+                    participantID: profile.id,
+                    displayName: profile.displayName,
+                    firstName: profile.name?.givenName,
+                    lastName: profile.name?.familyName,
+                    image: profile.photos![0].value
+                };
 
-				try {
-					let participant = await Participant.findOne({ participantID: profile.id });
+                try {
+                    let participant = await Participant.findOne({ participantID: profile.id });
 
-					if (participant) {
-						done(undefined, participant);
-					} else {
-						participant = await Participant.create(newUser);
-						done(undefined, participant);
-					}
-				} catch (err) {
-					logger.error(err);
-				}
-			}
-		)
-	);
-	passport.serializeUser((user: any, done: any) => {
-		done(null, user.id);
-	});
+                    if (participant) {
+                        done(undefined, participant);
+                    } else {
+                        participant = await Participant.create(newUser);
+                        done(undefined, participant);
+                    }
+                } catch (err) {
+                    logger.error(err);
+                }
+            }
+        )
+    );
+    passport.serializeUser((user: any, done: any) => {
+        done(null, user.id);
+    });
 
-	passport.deserializeUser((id: any, done: any) => {
-		Participant.findById(id, (err, user) => {
-			done(err, user);
-		});
-	});
+    passport.deserializeUser((id: any, done: any) => {
+        Participant.findById(id, (err: any, user: any) => {
+            done(err, user);
+        });
+    });
 };
